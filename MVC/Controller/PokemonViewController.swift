3b84b071f73acbba6a0a65683a3cb6afde0a6384
enigmak9 <enigmak9@protonmail.com>
2024-08-25 22:17:30 -0600

    first 151 pokemon added

--- MVC/Controller/PokemonViewController.swift
+++ MVC/Controller/PokemonViewController.swift
@@ -1,9 +1,3 @@
-//
-//  PokemonViewController.swift
-//  MVC
-//
-//
-
 import UIKit
 
 class PokemonViewController: UIViewController {
@@ -14,22 +8,23 @@ class PokemonViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Data will be fetched from the data manager upon the view's loading.
         dataManager.fetch()
-//        var i = 0
-//        while i < dataManager.countPokemons() {
-//            print(dataManager.getPokemon(at: i))
-//            i += 1
-//        }
+        
+        // Set the delegate and data source for the table view.
+        pokemonTableView.delegate = self
+        pokemonTableView.dataSource = self
     }
 }
 
-
-extension PokemonViewController : UITableViewDelegate, UITableViewDataSource {
+extension PokemonViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // The number of rows in each section of the table view will be returned.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return dataManager.countPokemons()
     }
     
+    // Cells will be populated with Pokémon data from the data manager.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "pokemonCell", for: indexPath) as! PokemonCell
         let pokemon = dataManager.getPokemon(at: indexPath.row)
@@ -38,12 +33,39 @@ extension PokemonViewController : UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
-//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//        return "Pokemon"
-//    }
-    
+    // A custom header view for the table section will be created, featuring a blurred Pokémon image.
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        return UIImageView.init(image: UIImage(named: "Pokemon"))
+        // A container view will be created to hold the header's content.
+        let containerView = UIView()
+        
+        // An image view will be created to display the Pokémon image.
+        let imgHeader = UIImageView(image: UIImage(named: "Pokemon"))
+        imgHeader.translatesAutoresizingMaskIntoConstraints = false
+        imgHeader.contentMode = .scaleAspectFill
+        imgHeader.clipsToBounds = true
+        
+        // A view for the blur effect will be created and applied on top of the image view.
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // The image view and blur view will be added to the container view.
+        containerView.addSubview(imgHeader)
+        containerView.addSubview(blurView)
+        
+        // Constraints will be set up to ensure the image and blur views cover the entire header area.
+        NSLayoutConstraint.activate([
+            imgHeader.topAnchor.constraint(equalTo: containerView.topAnchor),
+            imgHeader.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            imgHeader.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            imgHeader.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            
+            // blurView.topAnchor.constraint(equalTo: containerView.topAnchor),
+            // blurView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor),
+            // blurView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            // blurView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+        ])
+        
+        return containerView
     }
-
 }
